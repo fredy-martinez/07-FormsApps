e5c2f8ab7e312707539e1c6db95e18729e96de54
Fredy Amilcar Martinez Girón <fredy.martinez@senprende.hn>
2023-12-18 15:08:09 -0600

    Formularios reactivos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-basic-page',
@@ -6,6 +8,32 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
 
+  constructor( private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.myForm.reset({
+       
+    })
+  }
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3) ]],
+    price:[0, [Validators.required, Validators.min(1)]],
+    inStorage:[0, [Validators.required, Validators.min(0)]],
+  })
+
+  onSubmit(){
+
+    //No permite que vaya vacío
+    if (this.myForm.invalid) return;
+
+    console.log(this.myForm.value);
+
+    //Resetea el formulario, poniendole una cantidad al precio y al almacenaje
+    this.myForm.reset({price: 0, inStorage: 0});
+    
+  }
 }
+
--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    BasicPageComponent
+    BasicPageComponent,
+    DynamicPageComponent,
+    SwitchesPageComponent
   ],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class ReactiveModule { }
