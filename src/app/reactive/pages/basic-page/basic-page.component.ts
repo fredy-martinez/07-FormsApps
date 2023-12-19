6201cf934d854f6ee5bd299a023943cbe04cc40f
Fredy Amilcar Martinez Girón <fredy.martinez@senprende.hn>
2023-12-19 14:01:16 -0600

    Validaciones

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -13,21 +13,49 @@ export class BasicPageComponent implements OnInit{
   constructor( private fb: FormBuilder){}
 
   ngOnInit(): void {
-    this.myForm.reset({
-       
-    })
+    this.myForm.reset({ price: 0, inStorage: 0 })
   }
 
+  //getter del error
+  getFieldError( field: string): string | null{
+
+    //Si no existe el campo
+    if( !this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch( key ){
+        case 'required':
+          return 'Este campo es requerido'
+
+        case 'minlength':
+          return `Mínimo  ${ errors['minlength'].requiredLength} caracteres`
+      }
+    }
+    return null;
+  }
+
+  //Validación
+  isValidField( field: string): boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3) ]],
     price:[0, [Validators.required, Validators.min(1)]],
     inStorage:[0, [Validators.required, Validators.min(0)]],
   })
 
-  onSubmit(){
+  onSubmit(): void{ 
 
     //No permite que vaya vacío
-    if (this.myForm.invalid) return;
+    if ( this.myForm.invalid ) {      
+      //Al presionar guardar dispara las validaciones, or si un campo esta vacío
+      this.myForm.markAllAsTouched();
+      return;
+    }
 
     console.log(this.myForm.value);
 
--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class DynamicPageComponent {
 
+  constructor( private fb: FormBuilder){}
+
+  //Getter para obtener favorite games
+  get favoriteGames(){
+    return this.dynamicForm.get('favoriteGames') as FormArray;
+  }
+
+  public dynamicForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3) ]],
+    favoriteGames:this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['NFMW', Validators.required],
+    ]),
+  })
+
+  onSubmit(): void{
+    
+    if(this.dynamicForm.invalid) {
+
+      this.dynamicForm.markAllAsTouched();
+      return;
+    }
+
+    console.log( this.dynamicForm.value);
+
+    this.dynamicForm.reset
+  }
+  
 }
