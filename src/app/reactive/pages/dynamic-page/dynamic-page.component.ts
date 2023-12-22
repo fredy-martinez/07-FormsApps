b6c1f77219dd735a74165970be312d1235d4150a
Fredy Amilcar Martinez Girón <fredy.martinez@senprende.hn>
2023-12-22 13:12:03 -0600

    reactive pages

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -10,11 +10,7 @@ export class DynamicPageComponent {
 
   constructor( private fb: FormBuilder){}
 
-  //Getter para obtener favorite games
-  get favoriteGames(){
-    return this.dynamicForm.get('favoriteGames') as FormArray;
-  }
-
+  //Declaro el reactive form con sus validaciones
   public dynamicForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3) ]],
     favoriteGames:this.fb.array([
@@ -22,6 +18,44 @@ export class DynamicPageComponent {
       ['NFMW', Validators.required],
     ]),
   })
+
+  //Getter para obtener favorite games
+  get favoriteGames(){
+    return this.dynamicForm.get('favoriteGames') as FormArray;
+  }
+
+  //Getter del error
+  getFieldError( field: string): string | null{
+
+    //Si no existe el campo
+    if( !this.dynamicForm.controls[field]) return null;
+
+    const errors = this.dynamicForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch( key ){
+        case 'required':
+          return 'Este campo es requerido'
+
+        case 'minlength':
+          return `Mínimo  ${ errors['minlength'].requiredLength} caracteres`
+      }
+    }
+    return null;
+  }
+  
+    //Validación
+    isValidField( field: string): boolean | null {
+      return this.dynamicForm.controls[field].errors && this.dynamicForm.controls[field].touched;
+    }
+
+    //Validación del arreglo
+    isValidFieldInArray( formArray: FormArray, i: number){
+      return formArray.controls[i].errors 
+          && formArray.controls[i].touched;
+
+    }
+
 
   onSubmit(): void{
     
@@ -33,7 +67,7 @@ export class DynamicPageComponent {
 
     console.log( this.dynamicForm.value);
 
-    this.dynamicForm.reset
+    this.dynamicForm.reset();
   }
   
 }
